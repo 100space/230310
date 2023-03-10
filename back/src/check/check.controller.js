cb427a2e2d39a25ca1ed463c2783a2e47a11f3b5
cloudcoke <lms4296@gmail.com>
2023-03-10 16:08:25 +0900

    feat: logout 완료

--- back/src/check/check.controller.js
+++ back/src/check/check.controller.js
@@ -3,9 +3,9 @@ const service = require("./check.service");
 const login = (req, res, next) => {
   try {
     const { userid, userpw } = req.body;
-    const { token, ...result } = service.loginService({ userid, userpw });
-    const expires = new Date(result.exp * 1000);
-    if (token) return res.status(200).cookie("token", token, { expires }).json(result);
+    const { token, expire, ...result } = service.loginService({ userid, userpw });
+    const expires = new Date(expire * 1000);
+    if (token) return res.status(200).cookie("token", token, { expires, path: "/" }).json(result);
     res.status(401).json(result);
   } catch (e) {
     next(e);
@@ -18,12 +18,20 @@ const logout = (req, res, next) => {
     if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }));
     const [type, token] = auth.split(" ");
     if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }));
-    const result = service.logoutService({ token });
-    // res.clearCookie("token");
+    res.clearCookie("token");
     res.status(200).json({ message: "로그아웃 성공", status: 200, isLogin: false });
   } catch (e) {
     next(e);
   }
 };
 
-module.exports = { login, logout };
+const me = (req, res, next) => {
+  const auth = req.headers.authorization;
+  if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }));
+  const [type, token] = auth.split(" ");
+  if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }));
+  const result = service.getMe({ token });
+  res.status(200).json({ message: "회원정보 가져오기 성공", status: 200, ...result });
+};
+
+module.exports = { me, login, logout };
--- back/src/check/check.route.js
+++ back/src/check/check.route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./check.controller");
 
-router.get("/", (req, res, next) => controller.logout(req, res, next));
+router.get("/", (req, res, next) => controller.me(req, res, next));
 router.post("/", (req, res, next) => controller.login(req, res, next));
+router.delete("/", (req, res, next) => controller.logout(req, res, next));
 
 module.exports = router;
