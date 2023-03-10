d3c6d1da262b8c7e6c4c82f6f0b4e83365aa542b
BAEK'space <112856532+100space@users.noreply.github.com>
2023-03-10 17:48:13 +0900

    feat : token expire

--- back/src/check/check.controller.js
+++ back/src/check/check.controller.js
@@ -1,37 +1,38 @@
-const service = require("./check.service");
+const service = require("./check.service")
 
 const login = (req, res, next) => {
-  try {
-    const { userid, userpw } = req.body;
-    const { token, expire, ...result } = service.loginService({ userid, userpw });
-    const expires = new Date(expire * 1000);
-    if (token) return res.status(200).cookie("token", token, { expires, path: "/" }).json(result);
-    res.status(401).json(result);
-  } catch (e) {
-    next(e);
-  }
-};
+    try {
+        const { userid, userpw } = req.body
+        const { token, expire, ...result } = service.loginService({ userid, userpw })
+        const expires = new Date(expire * 1000)
+        if (token) return res.status(200).cookie("token", token, { expires, path: "/" }).json(result)
+        res.status(401).json(result)
+    } catch (e) {
+        next(e)
+    }
+}
 
 const logout = (req, res, next) => {
-  try {
-    const auth = req.headers.authorization;
-    if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }));
-    const [type, token] = auth.split(" ");
-    if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }));
-    res.clearCookie("token");
-    res.status(200).json({ message: "로그아웃 성공", status: 200, isLogin: false });
-  } catch (e) {
-    next(e);
-  }
-};
+    try {
+        const auth = req.headers.authorization
+        console.log(auth, 123123123)
+        if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }))
+        const [type, token] = auth.split(" ")
+        if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }))
+        res.clearCookie("token")
+        res.status(200).json({ message: "로그아웃 성공", status: 200, isLogin: false })
+    } catch (e) {
+        next(e)
+    }
+}
 
 const me = (req, res, next) => {
-  const auth = req.headers.authorization;
-  if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }));
-  const [type, token] = auth.split(" ");
-  if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }));
-  const result = service.getMe({ token });
-  res.status(200).json({ message: "회원정보 가져오기 성공", status: 200, ...result });
-};
+    const auth = req.headers.authorization
+    if (!auth) throw new Error(JSON.stringify({ message: "Authorization이 없습니다.", status: 401 }))
+    const [type, token] = auth.split(" ")
+    if (type.toLowerCase() !== "bearer") throw new Error(JSON.stringify({ message: "Authorization Type Error", status: 401 }))
+    const result = service.getMe({ token })
+    res.status(200).json({ message: "회원정보 가져오기 성공", status: 200, ...result })
+}
 
-module.exports = { me, login, logout };
+module.exports = { me, login, logout }
--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -1,11 +1,46 @@
 import styled from "styled-components"
+import axios from "axios"
+import { useStore } from "../store"
+import { useEffect, useState } from "react"
 
 const Div = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     font-size: 10rem;
+    & {
+        margin-bottom: 10rem;
+    }
 `
+
 export const Profile = () => {
-    return <Div>Profile</Div>
+    const { state } = useStore()
+    const dates = Math.floor(new Date() / 1000)
+    const [now, setNow] = useState(dates)
+    const { expire } = state.user
+    const check = (v) => {
+        const expire = v - dates
+
+        const diffInSeconds = expire
+
+        const hours = Math.floor(diffInSeconds / 3600)
+        const minutes = Math.floor((diffInSeconds % 3600) / 60)
+        const seconds = diffInSeconds % 60
+
+        const timeout = `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
+        return timeout
+    }
+    useEffect(() => {
+        setInterval(() => {
+            const dates = Math.floor(new Date() / 1000)
+            setNow(dates)
+        }, 1000)
+    }, [])
+    return (
+        <>
+            <Div>Profile</Div>
+            <Div>쿠키 만료까지 남은시간 </Div>
+            <Div> {check(expire)}</Div>
+        </>
+    )
 }
